9bf4bb5a53783224dd8176a8856412a7b01eadde
Harry Sharma <harrysharma1066@gmail.com>
2024-01-24 15:52:02 +0000

    add: symbolic to octal function

--- src/chmod.rs
+++ src/chmod.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-
+const BINDING: &str = "<Incorrect Value>";
 #[allow(dead_code)]
 pub struct Chmod{
 
@@ -23,11 +23,51 @@ impl Chmod{
         let group:u16 = (octal/10)%10;
         let user:u16  = (octal/100)%10;
 
-        let binding = "<Incorrect Value>".to_string();
+        let binding = BINDING.to_string();
         let user_string = octal_to_symbolic.get(&user).unwrap_or(&binding);
         let group_string = octal_to_symbolic.get(&group).unwrap_or(&binding);
         let owner_string = octal_to_symbolic.get(&owner).unwrap_or(&binding);
         print!("{}{}{}",user_string,group_string,owner_string);
+        println!("");
 
-    }   
+    } 
+
+    pub fn convert_symbolic_to_octal(&self,symbolic:String){
+        let mut symbolic_to_octal: HashMap<String, u16> = HashMap::new();
+        symbolic_to_octal.insert("---".to_string(), 0);
+        symbolic_to_octal.insert("--x".to_string(), 1);
+        symbolic_to_octal.insert("-w-".to_string(), 2);
+        symbolic_to_octal.insert("-wx".to_string(), 3);
+        symbolic_to_octal.insert("r--".to_string(), 4);
+        symbolic_to_octal.insert("r-x".to_string(), 5);
+        symbolic_to_octal.insert("rw-".to_string(), 6);
+        symbolic_to_octal.insert("rwx".to_string(), 7);
+        if symbolic.len()!=9{
+            println!("Not correct length");
+            return;
+        }
+        let user =&symbolic[0..3].to_string();
+        let group = &symbolic[3..6].to_string();
+        let owner = &symbolic[6..9].to_string();
+        
+        let binding = BINDING.to_string();
+
+        let user_octal = symbolic_to_octal.get(user);
+        let group_octal = symbolic_to_octal.get(group);
+        let owner_octal = symbolic_to_octal.get(owner);
+        match (user_octal,group_octal,owner_octal){
+            (None, None, None) => print!("{}{}{}",binding,binding,binding),
+            (None, None, Some(_)) => print!("{}{}{}",binding,binding,owner_octal.unwrap()),
+            (None, Some(_), None) => print!("{}{}{}",binding,group_octal.unwrap(),binding),
+            (None, Some(_), Some(_)) => print!("{}{}{}",binding,group_octal.unwrap(),owner_octal.unwrap()),
+            (Some(_), None, None) => print!("{}{}{}",user_octal.unwrap(),binding,binding),
+            (Some(_), None, Some(_)) => print!("{}{}{}",user_octal.unwrap(),binding,owner_octal.unwrap()),
+            (Some(_), Some(_), None) => print!("{}{}{}",user_octal.unwrap(),group_octal.unwrap(),binding),
+            (Some(_), Some(_), Some(_)) => print!("{}{}{}",user_octal.unwrap(),group_octal.unwrap(),owner_octal.unwrap()),
+        }
+
+        println!("");
+        
+
+    }  
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use chmod::Chmod;
 
 fn main() {
     let a = Chmod{};
-    a.convert_octal_to_symbolic(777);
+    a.convert_octal_to_symbolic(677);
+    a.convert_symbolic_to_octal("rwx-wx-xw".to_string());
 }
