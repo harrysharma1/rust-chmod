4fc6c6d58e2087be00ae770999a52c1d28543d5a
Harry Sharma <harrysharma1066@gmail.com>
2024-01-30 15:33:10 +0000

    fix: octal conversion to take 000

--- src/chmod.rs
+++ src/chmod.rs
@@ -28,7 +28,7 @@ impl Chmod{
     
     pub fn convert_octal_to_symbolic(&self,octal:u16)->String{
 
-        if octal<100 || octal>999{
+        if  octal>999{
             let error_incorrect_length = "<Err: Not correct length>\n".to_string();
             return error_incorrect_length;      
         }
